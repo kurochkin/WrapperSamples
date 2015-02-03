698f08cd02a686cfadab46cca5f9967871476f90
Ivan Kurochkin <ivan.kurochkin@gmail.com>
2015-02-03 21:04:00 +0300

    Add attributes

--- CodeWrapperGenerator/CodeWrapperGenerator/Program.cs
+++ CodeWrapperGenerator/CodeWrapperGenerator/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +17,8 @@ namespace CodeWrapperGenerator
 
             var sb = new StringBuilder();
             var currentDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (currentDir == null) throw new ArgumentException("path null");
+
             Assembly asmbl =
                 Assembly.LoadFile(Path.Combine(currentDir, "SrcLib.dll"));
             var classes = asmbl.GetTypes().Where(t => t.IsClass);
@@ -22,13 +26,39 @@ namespace CodeWrapperGenerator
             {
                 ProcessClass(@class, sb);
             }
-            Console.WriteLine(sb.ToString());
+
+            using (var sw = new StreamWriter(Path.Combine(currentDir, "out.cs"), false, Encoding.UTF8))
+            {
+                sw.Write(sb.ToString());
+            }
+            Console.WriteLine("Done");
             Console.ReadKey();
+        }
+
+        private static Guid GetDeterministicGuid(string input)
+        {
+
+            //use MD5 hash to get a 16-byte hash of the string: 
+
+            var provider = new MD5CryptoServiceProvider();
+            byte[] inputBytes = Encoding.Default.GetBytes(input);
+            byte[] hashBytes = provider.ComputeHash(inputBytes);
+            //generate a guid from the hash: 
+            var hashGuid = new Guid(hashBytes);
+
+            return hashGuid;
+
         }
 
         private static void ProcessClass(Type @class, StringBuilder sb)
         {
-
+            Console.WriteLine("Processing class: " + @class.FullName );
+            sb.AppendLine();
+            var guidClass = GetDeterministicGuid(@class.Name);
+            sb.AppendLine("[ComVisible(true)]");
+            sb.AppendLine("[InterfaceType(ComInterfaceType.InterfaceIsDual)]");
+            sb.AppendFormat("[Guid(\"{0}\")]\n", guidClass);
+            sb.AppendFormat("[ProgId(\"Pyramid.{0}\")]\n", @class.Name);
             sb.AppendFormat("public class {0} {{\n", @class.Name);
             sb.AppendFormat("private {0} _wrappedInstance;\n", @class.FullName);
             var properties = @class.GetProperties();
